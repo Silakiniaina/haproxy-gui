5c6289bc77610e61d674e764982a1a51c531c7d7
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-03 22:35:42 +0300

    feat : Create function generate_config .

--- services/config_generator.py
+++ services/config_generator.py
@@ -5,3 +5,17 @@ from services.storage_service import StorageService
 class HAProxyConfigGenerator:
     def __init__(self, storage_service: StorageService):
         self.storage = storage_service
+
+    def generate_config(self) -> str:
+        config_lines = []
+        
+        config_lines.extend(self._generate_global_section())
+        config_lines.extend(self._generate_defaults_section())
+        
+        for frontend in self.storage.frontends.values():
+            config_lines.extend(self._generate_frontend_config(frontend))
+        
+        for backend in self.storage.backends.values():
+            config_lines.extend(self._generate_backend_config(backend))
+        
+        return "\n".join(config_lines)
