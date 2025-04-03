14739156a2ecf8d035298fe77883e08b733141b0
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-03 22:31:26 +0300

    feat : Create endpoint /config to show config.

--- routes/config_routes.py
+++ routes/config_routes.py
@@ -6,3 +6,8 @@ config_bp = Blueprint('config', __name__)
 
 # Global config generator variable
 config_generator = None
+
+@config_bp.route('/config')
+def show_config():
+    config = config_generator.generate_config()
+    return render_template('config.html', config=config)
