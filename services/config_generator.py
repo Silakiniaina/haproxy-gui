f23714986ce1e65fe24f70c977355258f4d38039
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-03 22:37:40 +0300

    feat : Create function _generate_backend_config to generate backend config.

--- services/config_generator.py
+++ services/config_generator.py
@@ -64,3 +64,59 @@ class HAProxyConfigGenerator:
             config_lines.append(f"    default_backend {backend}")
         config_lines.append("")
         return config_lines
+
+    def _generate_backend_config(self, backend: Backend) -> list:
+        config_lines = [
+            f"backend {backend.name}",
+            f"    mode {backend.mode}",
+            f"    balance {backend.balance}"
+        ]
+        if backend.timeout_connect:
+            config_lines.append(f"    timeout connect {backend.timeout_connect}")
+        if backend.timeout_server:
+            config_lines.append(f"    timeout server {backend.timeout_server}")
+        if backend.timeout_check:
+            config_lines.append(f"    timeout check {backend.timeout_check}")
+        if backend.check_enabled:
+            config_lines.append("    option httpchk")
+            if backend.option_httpchk:
+                config_lines.append(f"    http-check {backend.option_httpchk}")
+        if backend.maxconn:
+            config_lines.append(f"    maxconn {backend.maxconn}")
+        if backend.retries:
+            config_lines.append(f"    retries {backend.retries}")
+        if backend.cookie:
+            config_lines.append(f"    cookie {backend.cookie} {backend.cookie_type}")
+        if backend.forwardfor:
+            config_lines.append("    option forwardfor")
+        if backend.option_redispatch:
+            config_lines.append("    option redispatch")
+        
+        for server in backend.servers.values():
+            server_line = f"    server {server.name} {server.host}:{server.port}"
+            if server.weight != 1:
+                server_line += f" weight {server.weight}"
+            if server.check:
+                server_line += " check"
+                if server.check_port:
+                    server_line += f" port {server.check_port}"
+                if server.check_inter:
+                    server_line += f" inter {server.check_inter}"
+                if server.check_fall:
+                    server_line += f" fall {server.check_fall}"
+                if server.check_rise:
+                    server_line += f" rise {server.check_rise}"
+            if server.maxconn:
+                server_line += f" maxconn {server.maxconn}"
+            if server.backup:
+                server_line += " backup"
+            if server.disabled:
+                server_line += " disabled"
+            if server.maintenance:
+                server_line += " maintenance"
+            if backend.cookie:
+                server_line += f" cookie {server.name}"
+            config_lines.append(server_line)
+        
+        config_lines.append("")
+        return config_lines
