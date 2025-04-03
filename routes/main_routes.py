e2a9bf15ebaede5c162e2a8195589b7664222792
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-03 22:44:14 +0300

    feat : Create endpoint  /

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -2,3 +2,13 @@ from flask import Blueprint, render_template
 from services.storage_service import StorageService
 
 main_bp = Blueprint('main', __name__)
+
+@main_bp.route('/')
+def index():
+    # Check status of all servers before rendering
+    for backend in storage_service.backends.values():
+        for server in backend.servers.values():
+            server.check_status()
+    return render_template('index.html', 
+                         frontends=storage_service.frontends, 
+                         backends=storage_service.backends)
